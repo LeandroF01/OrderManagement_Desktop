5baf66b225bd80c4280f3fc17cde66a4fb6693fd
LeandroF01 <leaylio3327@gmail.com>
2024-08-08 18:28:39 -0300

    Add models Payment, Products and Users

---
+++ OrderManagement_Desktop/Models/PaymentMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class PaymentMethods
+    {
+        public int PaymentID { get; set; }
+        public int OrderID { get; set; }
+        public string PaymentType { get; set; } 
+        public decimal Amount { get; set; }
+    }
+}
---
+++ OrderManagement_Desktop/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Users
+    {    
+            public int UserID { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string Phone { get; set; }
+            public string Address { get; set; }
+            public string UserType { get; set; }      
+    }
+}
