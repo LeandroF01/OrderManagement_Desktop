e641e469c1064492b4bce7b721c0be59e33e14a5
LeandroF01 <leaylio3327@gmail.com>
2024-08-08 18:27:44 -0300

    Add models Addresses and Categories

---
+++ OrderManagement_Desktop/Models/Addresses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Addresses
+    {
+        public int AddressID { get; set; }
+        public int UserID { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ OrderManagement_Desktop/Models/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Categories
+    {        
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
