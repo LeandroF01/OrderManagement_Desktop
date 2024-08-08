4abcd865c44dc911b5ceedad8320c7fcb5246191
LeandroF01 <leaylio3327@gmail.com>
2024-08-08 18:28:18 -0300

    Add models Oders and OrderDetails

---
+++ OrderManagement_Desktop/Models/OrderDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class OrderDetails
+    {
+        public int OrderDetailID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ OrderManagement_Desktop/Models/Orders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Orders
+    {
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string OrderType { get; set; } 
+        public decimal Total { get; set; }
+
+    }
+}
