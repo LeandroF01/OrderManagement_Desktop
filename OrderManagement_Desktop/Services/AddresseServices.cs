6203d643537187a93ce4a74a58b152e47cd3a0a9
LeandroF01 <leaylio3327@gmail.com>
2024-08-09 16:49:31 -0300

    Add services Addresses and Categories

---
+++ OrderManagement_Desktop/Services/AddresseServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class AddresseServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public AddresseServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<Addresses>> GetAddresses()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Addresses>>("Addresses");
+        }
+
+        public async Task<Addresses> GetAddresById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Addresses>($"Addresses/{id}");
+        }
+
+        public async Task<bool> AddAddres(Addresses addresses)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Addresses", addresses);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateAddres(Addresses addresses)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Addresses/{addresses.AddressID}", addresses);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteAddres(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Addresses/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ OrderManagement_Desktop/Services/CategorieServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class CategorieServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategorieServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<Categories>> GetCategories()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Categories>>("Categories");
+        }
+
+        public async Task<Categories> GetCategorieById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Categories>($"Categories/{id}");
+        }
+
+        public async Task<bool> AddCategorie(Categories categories)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Categories", categories);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateCategorie(Categories categories)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Categories/{categories.CategoryID}", categories);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteCategorie(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Categories/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
