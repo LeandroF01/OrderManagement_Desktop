e9a78f1ce31678cd2914ac8964a61e2938173d1a
LeandroF01 <leaylio3327@gmail.com>
2024-08-09 16:49:48 -0300

    Add services Orders and OrderDetails

---
+++ OrderManagement_Desktop/Services/OrderDetailServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class OrderDetailServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderDetailServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<OrderDetails>> GetOrderDetails()
+        {
+            return await _httpClient.GetFromJsonAsync<List<OrderDetails>>("OrderDetails");
+        }
+
+        public async Task<OrderDetails> GetOrderDetailById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<OrderDetails>($"OrderDetails/{id}");
+        }
+
+        public async Task<bool> AddOrderDetail(OrderDetails orderDetail)
+        {
+            var response = await _httpClient.PostAsJsonAsync("OrderDetails", orderDetail);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateOrderDetail(OrderDetails orderDetail)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"OrderDetails/{orderDetail.OrderDetailID}", orderDetail);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteOrderDetail(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"OrderDetails/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ OrderManagement_Desktop/Services/OrderServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class OrderServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<Orders>> GetOrders()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Orders>>("Orders");
+        }
+
+        public async Task<Orders> GetOrderById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Orders>($"Orders/{id}");
+        }
+
+        public async Task<bool> AddOrder(Orders orders)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Orders", orders);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateOrder(Orders orders)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Orders/{orders.OrderID}", orders);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteOrder(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Orders/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
