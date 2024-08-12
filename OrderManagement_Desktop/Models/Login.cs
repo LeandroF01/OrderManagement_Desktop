ceb8b20d33feb4e5a2b10d5ad64eba9ff9d92e84
LeandroF01 <leaylio3327@gmail.com>
2024-08-12 15:20:47 -0300

    Add models Login and AuthResponse

---
+++ OrderManagement_Desktop/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
