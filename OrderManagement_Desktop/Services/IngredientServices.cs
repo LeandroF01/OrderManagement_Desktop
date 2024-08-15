5ef0ac2c01c049ef0be30cc6ed375a797bdad881
LeandroF01 <leaylio3327@gmail.com>
2024-08-15 18:09:18 -0300

    Add services of ProductIngredients and Ingredients

---
+++ OrderManagement_Desktop/Services/IngredientServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class IngredientServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public IngredientServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<Ingredients>> GetIngredients()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Ingredients>>("Ingredients");
+        }
+
+        public async Task<Ingredients> GetIngredientById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Ingredients>($"Ingredients/{id}");
+        }
+
+        public async Task<bool> AddIngredient(Ingredients ingredient)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Ingredients", ingredient);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateIngredient(Ingredients ingredient)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"Ingredients/{ingredient.IngredientID}", ingredient);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteIngredient(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Ingredients/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ OrderManagement_Desktop/Services/ProductIngredientServices.cs
@@ -0,0 +1,49 @@
+﻿using OrderManagement_Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Services
+{
+    internal class ProductIngredientServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductIngredientServices()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5287/api/");
+        }
+
+        public async Task<List<ProductIngredients>> GetProductIngredients()
+        {
+            return await _httpClient.GetFromJsonAsync<List<ProductIngredients>>("ProductIngredients");
+        }
+
+        public async Task<ProductIngredients> GetProductIngredientById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<ProductIngredients>($"ProductIngredients/{id}");
+        }
+
+        public async Task<bool> AddProductIngredient(ProductIngredients productIngredients)
+        {
+            var response = await _httpClient.PostAsJsonAsync("ProductIngredients", productIngredients);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateProductIngredient(ProductIngredients productIngredients)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"ProductIngredients/{productIngredients.ProductIngredientID}", productIngredients);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteProductIngredient(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"ProductIngredients/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
