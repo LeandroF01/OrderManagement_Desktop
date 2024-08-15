90ac49f917376c72786d0a06e315e06e051e7b26
LeandroF01 <leaylio3327@gmail.com>
2024-08-15 17:35:07 -0300

    Refactor model of products

--- OrderManagement_Desktop/Models/Products.cs
+++ OrderManagement_Desktop/Models/Products.cs
@@ -14,8 +14,7 @@ namespace OrderManagement_Desktop.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Category { get; set; }
-        public int Stock { get; set; }
+        public string CategoryID { get; set; }
         public string ImageURL { get; set; }
     }
 }
