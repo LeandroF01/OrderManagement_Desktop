56f4b6619302d19e375d1d630b12afe534119b0c
LeandroF01 <leaylio3327@gmail.com>
2024-08-15 17:34:57 -0300

    Add models Ingredients and ProductsIngredientes

---
+++ OrderManagement_Desktop/Models/Ingredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Ingredients
+    {
+        public int IngredientID { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public int Unit { get; set; }
+    }
+}
---
+++ OrderManagement_Desktop/Models/ProductIngredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class ProductIngredients
+    {
+        public int ProductIngredientID { get; set; }
+        public int ProductID { get; set; }
+        public int IngredientID { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
