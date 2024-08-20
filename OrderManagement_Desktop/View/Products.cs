8a183af13e18eae5342491f821fc09fd6c607232
LeandroF01 <leaylio3327@gmail.com>
2024-08-20 19:15:44 -0300

    Add funtion add products and UpdateProduct

--- OrderManagement_Desktop/View/Products.cs
+++ OrderManagement_Desktop/View/Products.cs
@@ -1,4 +1,5 @@
 ﻿using OrderManagement_Desktop.Services;
+using OrderManagement_Desktop.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace OrderManagement_Desktop.View
 {
@@ -24,8 +26,6 @@ namespace OrderManagement_Desktop.View
             private void Products_Load(object sender, EventArgs e)
             {
                 ViewProducts();
-
-
             }
 
         private async void  ViewProducts()
@@ -40,6 +40,73 @@ namespace OrderManagement_Desktop.View
               MessageBox.Show($"Error al cargar los productos: {ex.Message}");
             }
         }
+
+        private async void AddProduct()
+        {
+            try
+            {
+                var newProduct = new Models.Products
+                {
+                    Name = "",
+                    Description = "",
+                    Price = 0,
+                    CategoryID = 0,
+                    Stock = 0,
+                    ImageURL = ""
+                }; 
+
+                var result = await _productServices.AddProduct(newProduct);
+
+                if (result)
+                {
+                    MessageBox.Show("Producto añadido con éxito.");
+                    ViewProducts(); 
+                }
+                else
+                {
+                    MessageBox.Show("Error al añadir el producto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private async void UpdateProduct()
+        {
+            try
+            {
+
+                var updatedProduct = new Models.Products
+                {
+                    Name = "",
+                    Description = "",
+                    Price = 0,
+                    CategoryID = 0,
+                    Stock = 0,
+                    ImageURL = ""
+                };
+
+                var result = await _productServices.UpdateProduct(updatedProduct);
+
+                if (result)
+                {
+                    MessageBox.Show("Producto actualizado con éxito.");
+                    ViewProducts();
+                }
+                else
+                {
+                    MessageBox.Show("Error al actualizar el producto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+
 
 
     }
