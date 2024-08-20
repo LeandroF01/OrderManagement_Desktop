0c7d3b0ed220c601ba5345314a651b3857447ff4
LeandroF01 <leaylio3327@gmail.com>
2024-08-20 19:01:55 -0300

    Add form Categories

---
+++ OrderManagement_Desktop/View/Categories.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace OrderManagement_Desktop.View
+{
+    partial class Categories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCategories = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)DataGridViewCategories).BeginInit();
+            SuspendLayout();
+            // 
+            // DataGridViewCategories
+            // 
+            DataGridViewCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGridViewCategories.Location = new Point(48, 57);
+            DataGridViewCategories.Name = "DataGridViewCategories";
+            DataGridViewCategories.Size = new Size(306, 274);
+            DataGridViewCategories.TabIndex = 0;
+            // 
+            // Categories
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(428, 450);
+            Controls.Add(DataGridViewCategories);
+            Name = "Categories";
+            Text = "Categories";
+            Load += Categories_Load;
+            ((System.ComponentModel.ISupportInitialize)DataGridViewCategories).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView DataGridViewCategories;
+    }
+}
---
+++ OrderManagement_Desktop/View/Categories.cs
@@ -0,0 +1,43 @@
+﻿using OrderManagement_Desktop.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderManagement_Desktop.View
+{
+    public partial class Categories : Form
+    {
+        private CategorieServices _categoriesServices;
+
+        public Categories()
+        {
+            InitializeComponent();
+            _categoriesServices = new CategorieServices();
+        }
+
+        private void Categories_Load(object sender, EventArgs e)
+        {
+            ViewCategories();
+        }
+
+
+        private async void ViewCategories()
+        {
+            try
+            {
+                var categoriesList = await _categoriesServices.GetCategories();
+                DataGridViewCategories.DataSource = categoriesList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los productos: {ex.Message}");
+            }
+        }
+    }
+}
