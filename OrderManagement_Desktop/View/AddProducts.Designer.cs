2fcc7a5b6d7abe619a87200217cdd2711cae3f5f
LeandroF01 <leaylio3327@gmail.com>
2024-08-21 18:52:38 -0300

    Add view AddProducts

---
+++ OrderManagement_Desktop/View/AddProducts.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace OrderManagement_Desktop.View
+{
+    partial class AddProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            ComboBoxCategories = new ComboBox();
+            richTextBox1 = new RichTextBox();
+            ListBoxIngredientes = new ListBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(233, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(40, 154);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(233, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // ComboBoxCategories
+            // 
+            ComboBoxCategories.FormattingEnabled = true;
+            ComboBoxCategories.Location = new Point(40, 213);
+            ComboBoxCategories.Name = "ComboBoxCategories";
+            ComboBoxCategories.Size = new Size(233, 23);
+            ComboBoxCategories.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.Location = new Point(40, 88);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(233, 43);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // ListBoxIngredientes
+            // 
+            ListBoxIngredientes.FormattingEnabled = true;
+            ListBoxIngredientes.ItemHeight = 15;
+            ListBoxIngredientes.Location = new Point(579, 66);
+            ListBoxIngredientes.Name = "ListBoxIngredientes";
+            ListBoxIngredientes.Size = new Size(120, 94);
+            ListBoxIngredientes.TabIndex = 7;
+            // 
+            // AddProducts
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ListBoxIngredientes);
+            Controls.Add(richTextBox1);
+            Controls.Add(ComboBoxCategories);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Name = "AddProducts";
+            Text = "AddProducts";
+            Load += AddProducts_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private ComboBox ComboBoxCategories;
+        private RichTextBox richTextBox1;
+        private ListBox ListBoxIngredientes;
+    }
+}
---
+++ OrderManagement_Desktop/View/AddProducts.cs
@@ -0,0 +1,156 @@
+﻿using OrderManagement_Desktop.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderManagement_Desktop.View
+{
+    public partial class AddProducts : Form
+    {
+
+        private ProductServices _productServices;
+        private ProductIngredientServices _productIngredientServices;
+        private CategorieServices _categoriesServices;
+        private IngredientServices _ingredientsServices;
+
+        public AddProducts()
+        {
+            InitializeComponent();
+            _productServices = new ProductServices();
+            _productIngredientServices = new ProductIngredientServices();
+            _categoriesServices = new CategorieServices();
+            _ingredientsServices = new IngredientServices();
+        }
+
+      //  ListBoxIngredientes
+
+        private async void AddProducts_Load(object sender, EventArgs e)
+        {
+           await ViewCategories();
+
+            await LoadIngredients();
+        }
+
+        private async Task ViewCategories()
+        {
+            try
+            {
+                var categoryList = await _categoriesServices.GetCategories();
+
+                ComboBoxCategories.DisplayMember = "Name"; // La propiedad que quieres mostrar.
+                ComboBoxCategories.ValueMember = "CategoryID"; // El valor asociado a cada ítem.
+
+                ComboBoxCategories.DataSource = categoryList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar las categorías: {ex.Message}");
+            }
+        }
+
+
+        private async void AddProduct()
+        {
+            try
+            {
+                var newProduct = new Models.Products
+                {
+                    Name = "",
+                    Description = "",
+                    Price = 0,
+                    CategoryID = 0,
+                    Stock = 0,
+                    ImageURL = ""
+                };
+
+                var productIngredients = new Models.ProductIngredients
+                {
+                    ProductID = 0,
+                    IngredientID = 0,
+                    Quantity = 0,
+                };
+
+                var result = await _productServices.AddProduct(newProduct);
+
+                if (result)
+                {
+                    MessageBox.Show("Producto añadido con éxito.");
+
+                    Products productsForm = new Products();
+                    productsForm.ViewProducts();
+                }
+                else
+                {
+                    MessageBox.Show("Error al añadir el producto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private async void UpdateProduct()
+        {
+            try
+            {
+
+                var updatedProduct = new Models.Products
+                {
+                    Name = "",
+                    Description = "",
+                    Price = 0,
+                    CategoryID = 0,
+                    Stock = 0,
+                    ImageURL = ""
+                };
+
+                var result = await _productServices.UpdateProduct(updatedProduct);
+
+                if (result)
+                {
+                    MessageBox.Show("Producto actualizado con éxito.");
+
+                    Products productsForm = new Products();
+                    productsForm.ViewProducts();
+                }
+                else
+                {
+                    MessageBox.Show("Error al actualizar el producto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private async Task LoadIngredients()
+        {
+            try
+            {
+                // Obtén la lista de ingredientes desde el servicio.
+                var ingredientsList = await _ingredientsServices.GetIngredients();
+
+                // Asigna la lista como el DataSource del ListBox.
+                ListBoxIngredientes.DataSource = ingredientsList;
+
+                // Especifica qué propiedad del objeto "Ingredient" se mostrará en el ListBox.
+                ListBoxIngredientes.DisplayMember = "Name"; // Propiedad del modelo de ingredientes (puede ser "Name" o cualquier otra).
+
+                // Si necesitas la ID del ingrediente para otras operaciones, puedes usar la propiedad ValueMember.
+                ListBoxIngredientes.ValueMember = "IngredientID";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los ingredientes: {ex.Message}");
+            }
+        }
+    }
+}
