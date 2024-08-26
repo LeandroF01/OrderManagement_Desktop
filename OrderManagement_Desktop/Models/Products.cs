54fdb3c87293e796f4779ba3442d47b250a88a30
LeandroF01 <leaylio3327@gmail.com>
2024-08-26 19:06:49 -0300

    Refactor model Products jsonignore

--- OrderManagement_Desktop/Models/Products.cs
+++ OrderManagement_Desktop/Models/Products.cs
@@ -5,17 +5,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace OrderManagement_Desktop.Models
 {
     internal class Products
     {
+        [JsonIgnore]
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int CategoryID { get; set; }
+        [JsonIgnore]
         public decimal Stock { get; set; }
+        [JsonIgnore]
         public string ImageURL { get; set; }
 
     }
