62b7cd3aa4cce138e7cf18e5abf31a9f0e10cbe7
LeandroF01 <leaylio3327@gmail.com>
2024-08-26 19:12:26 -0300

    Add utils ConfigurationHelper

---
+++ OrderManagement_Desktop/Utils/ConfigurationHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Utils
+{
+    internal class ConfigurationHelper
+    {
+        public static string GetTokenFromConfig()
+        {
+            return ConfigurationManager.AppSettings["AuthToken"];
+        }
+
+        public static void SaveTokenToConfig(string token)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["AuthToken"].Value = token;
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
