f03b296cf4f25b0107aad8f9e5d66a4073136a8d
LeandroF01 <leaylio3327@gmail.com>
2024-08-28 19:09:56 -0300

    Add ProductDOT to models

--- OrderManagement_Desktop/Models/ProductDOT.cs
+++ OrderManagement_Desktop/Models/ProductDOT.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.Json.Serialization;
 
 namespace OrderManagement_Desktop.Models
 {
-    internal class Products
+    internal class ProductDOT
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int CategoryID { get; set; }
-        public decimal Stock { get; set; }
         public string ImageURL { get; set; }
     }
 }
