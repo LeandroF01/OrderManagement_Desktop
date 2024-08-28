186d5612f3fc96dbc1297a45505a71ae9a2c296d
LeandroF01 <leaylio3327@gmail.com>
2024-08-28 19:10:07 -0300

    Add ProductIngredientDOTto models

---
+++ OrderManagement_Desktop/Models/ProductIngredientDOT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class ProductIngredientDOT
+    {
+        public int ProductID { get; set; }
+        public int IngredientID { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
