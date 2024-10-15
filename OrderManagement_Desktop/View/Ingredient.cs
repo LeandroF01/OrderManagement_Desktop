47d84ca23b0ffe5e72c47019facf974d6aa1381b
LeandroF01 <leaylio3327@gmail.com>
2024-10-15 11:55:02 -0300

    Add funtion AddIngredients and Delete

--- OrderManagement_Desktop/View/Ingredient.cs
+++ OrderManagement_Desktop/View/Ingredient.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -43,6 +44,98 @@ namespace OrderManagement_Desktop.View
             {
                 MessageBox.Show($"Error al cargar los ingretients: {ex.Message}");
             }
+        }
+
+        private async void AddIngredients()
+        {
+            var newIngredients = new Models.Ingredients
+            {
+                Name = TextBoxName.Text,
+                Quantity = Convert.ToDecimal(TextBoxQuantity.Text),
+                Unit = TextBoxUnit.Text
+
+            };
+
+            var json = JsonSerializer.Serialize(newIngredients);
+
+            var result = await _ingredientServicesToken.AddIngredient(newIngredients);
+
+            if (result)
+            {
+                MessageBox.Show("New Ingredients add");
+            }
+            else
+            {
+                MessageBox.Show("Error al añadir el Ingredients.");
+            }
+
+        }
+
+        private void ButtonAddIngredient_Click(object sender, EventArgs e)
+        {
+            PanelAddIngredient.Visible = true;
+        }
+
+        private async void ButtonDeleteIngredient_Click(object sender, EventArgs e)
+        {
+            if (DataGridViewIngredients.SelectedRows.Count > 0)
+            {
+                // Obtener el CategoryID de la categoría seleccionada
+                var selectedRow = DataGridViewIngredients.SelectedRows[0];
+                var ingredientID = Convert.ToInt32(selectedRow.Cells["IngredientID"].Value);
+
+                // Confirmar eliminación
+                var confirmation = MessageBox.Show("¿Estás seguro de que quieres eliminar esta ingredient?",
+                                                    "Confirmación de eliminación",
+                                                    MessageBoxButtons.YesNo);
+
+                if (confirmation == DialogResult.Yes)
+                {
+                    // Intentar eliminar la categoría
+                    try
+                    {
+                        var result = await _ingredientServicesToken.DeleteIngredient(ingredientID);
+
+                        if (result)
+                        {
+                            MessageBox.Show("Ingredients eliminada exitosamente.");
+                            ViewIngredients(); // Refrescar el DataGridView después de la eliminación
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al eliminar la Ingredients.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una categoría para eliminar.");
+            }
+
+        }
+
+        private void ButtonConfirm_Click(object sender, EventArgs e)
+        {
+            AddIngredients();
+            ResetValuesTextBox();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            PanelAddIngredient.Visible = false;
+            ResetValuesTextBox();
+        }
+
+        private void ResetValuesTextBox()
+        {
+            TextBoxName.Text = "";
+            TextBoxQuantity.Text = "";
+            TextBoxUnit.Text = "";
         }
     }
 }
