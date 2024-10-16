2f9de2a0cfa3000d644afa9868d38ce6154146aa
LeandroF01 <leaylio3327@gmail.com>
2024-10-16 17:24:26 -0300

    Change method to open form

--- OrderManagement_Desktop/View/Home.cs
+++ OrderManagement_Desktop/View/Home.cs
@@ -17,12 +17,6 @@ namespace OrderManagement_Desktop.View
             InitializeComponent();
         }
 
-        private void ButtonViewProducts_Click(object sender, EventArgs e)
-        {
-            Products productsForm = new Products();
-
-            productsForm.ShowDialog();
-        }
 
         private void Home_Load(object sender, EventArgs e)
         {
@@ -34,6 +28,59 @@ namespace OrderManagement_Desktop.View
             Orders ordersForm = new Orders();
 
             ordersForm.ShowDialog();
+        }
+
+        private Form activeForm = null;
+
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            // Asegúrate de que el nombre del panel coincida con el del diseñador.
+            panel2.Controls.Add(childForm);
+            panel2.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+
+        private void ButtonHome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonProducts_Click(object sender, EventArgs e)
+        {
+           openChildForm(new Products());
+
+        }
+
+        private void ButtonOrders_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTables_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonPayments_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonConfiguration_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
