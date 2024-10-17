23dd2c4377b430931d95c2092c1f19bc49a2db8b
LeandroF01 <leaylio3327@gmail.com>
2024-10-17 17:10:41 -0300

    Change desinger orders

--- OrderManagement_Desktop/View/Orders.Designer.cs
+++ OrderManagement_Desktop/View/Orders.Designer.cs
@@ -28,72 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewOrders = new DataGridView();
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
-            ((System.ComponentModel.ISupportInitialize)DataGridViewOrders).BeginInit();
-            tabControl1.SuspendLayout();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             SuspendLayout();
             // 
-            // DataGridViewOrders
+            // flowLayoutPanel1
             // 
-            DataGridViewOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            DataGridViewOrders.Location = new Point(48, 56);
-            DataGridViewOrders.Name = "DataGridViewOrders";
-            DataGridViewOrders.Size = new Size(304, 406);
-            DataGridViewOrders.TabIndex = 0;
-            // 
-            // tabControl1
-            // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(470, 104);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(356, 312);
-            tabControl1.TabIndex = 1;
-            // 
-            // tabPage1
-            // 
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(348, 284);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
-            tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(192, 284);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            flowLayoutPanel1.Location = new Point(101, 51);
+            flowLayoutPanel1.MinimumSize = new Size(600, 400);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(600, 400);
+            flowLayoutPanel1.TabIndex = 1;
             // 
             // Orders
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(977, 505);
-            Controls.Add(tabControl1);
-            Controls.Add(DataGridViewOrders);
+            ClientSize = new Size(922, 511);
+            Controls.Add(flowLayoutPanel1);
             Name = "Orders";
             Text = " ";
             Load += Orders_Load;
-            ((System.ComponentModel.ISupportInitialize)DataGridViewOrders).EndInit();
-            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView DataGridViewOrders;
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
