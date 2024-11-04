a1f3e934a7d38cebe2e12a9e852bca5ea85c052c
LeandroF01 <leaylio3327@gmail.com>
2024-11-04 10:47:56 -0300

    Add models Tables

---
+++ OrderManagement_Desktop/Models/Tables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement_Desktop.Models
+{
+    internal class Tables
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public string Status { get; set; } = "Available"; // Default value
+        public int Capacity { get; set; }
+    }
+}
